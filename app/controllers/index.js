a581c8a8fc5d36470821e79c334edced0f26e3f8
David Lai <me@dlai0001.com>
2015-07-06 15:09:17 -0700

    sound effects

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,12 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     actions: {
-        test: function() {
-            var newPost = this.store.createRecord('post', {
-                title: 'EmberFire is flaming hot!',
-                body: 'You can store and sync data in realtime without a backend.'
-            });
-            newPost.save();
+        error: function() {
+//            var newPost = this.store.createRecord('post', {
+//                title: 'EmberFire is flaming hot!',
+//                body: 'You can store and sync data in realtime without a backend.'
+//            });
+//            newPost.save();
+
+            console.log("test");
+            playError();
+        },
+
+        success: function() {
+            playSuccess();
         }
     }
 });
